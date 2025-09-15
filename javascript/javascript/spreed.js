49b932a244202cd541b057e0d0cc8e8e81282490
paulsunny90 <paulsunny87267@gmail.com>
2025-09-15 22:28:10 +0530

    loop

--- javascript/javascript/spreed.js
+++ javascript/javascript/spreed.js
@@ -11,3 +11,18 @@ let[a,b,c,d,e,f]=arr2
 console.log(a,b,c,d,e,f);
 
 console.log(this)
+
+
+let list1=[1,10,7,4,6]
+n=list1.length
+for(i=0;i<n;i++){
+    for(j=i+1;j<n;j++){
+        if(list1[i]>list1[j+1]){
+            let temp=list1[j+1]
+            list1[j+1]=list1[j]
+            list1[j]=temp
+        }
+    }
+}
+console.log(list1);
+console.log(list1[n-1]);
